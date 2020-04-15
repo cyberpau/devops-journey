3829c5f82df692f3b883879340c01fae2f311ec4
cyberpau <johnpaulomataac@gmail.com>
2020-04-15 18:15:39 +0800

    fix calico manifest

---
+++ network-policies/Flannel/apply.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Authored by John Paulo Mataac
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
